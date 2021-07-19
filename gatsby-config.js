fe81e53389536333aec3c0c98427b0205df4c041
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2021-07-19 15:39:02 +0530

    testing windows resize

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,10 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
+    pathPrefix: "/gatsby-trial"
+  },
+  flags: {
+    DEV_SSR: true
   },
   plugins: [],
 };
